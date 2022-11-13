6858d4720b441ecfa6ff475c3fd9de8c5138952e
kawaym <kawayrmarinho@poli.ufrj.br>
2022-11-13 15:28:49 -0300

    feat: adds winner check

--- src/input.rs
+++ src/input.rs
@@ -1,12 +1,7 @@
 use std::io::{self};
 
 pub fn get_input_choice() -> String {
-    let mut buff = String::new();
-    io::stdin()
-        .read_line(&mut buff)
-        .expect("reading from stdin failed");
-
-    let word = buff.trim().to_string();
+    let word = get_input_from_stdin();
 
     if word.len() > 1 || (word.to_uppercase() != "X" && word.to_uppercase() != "O") {
         println!("input should be a X or O");
@@ -22,11 +17,8 @@ pub fn get_input_choice() -> String {
 }
 
 pub fn get_input_coord() -> Vec<usize> {
-    let mut buff = String::new();
-    io::stdin()
-        .read_line(&mut buff)
-        .expect("reading from stdin failed");
-    let binding = buff.trim().to_string();
+    println!("Primeiro número deve ser a linha e o segundo a coluna alvo");
+    let binding = get_input_from_stdin();
 
     if binding.len() > 3 {
         println!("input should be no more than 3 characters");
@@ -36,8 +28,18 @@ pub fn get_input_coord() -> Vec<usize> {
     let coord: Vec<usize> = binding
         .split(" ")
         .into_iter()
-        .map(|c| c.parse::<usize>().unwrap())
+        .map(|c| c.parse::<usize>().unwrap() - 1)
         .collect();
 
     return coord;
+}
+
+fn get_input_from_stdin() -> String {
+    let mut buff = String::new();
+    io::stdin()
+        .read_line(&mut buff)
+        .expect("reading from stdin failed");
+    let binding = buff.trim().to_string();
+
+    return binding;
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -43,5 +43,98 @@ pub fn insert_letter(matrix: &mut Vec<Vec<String>>, letter: String, coord: Vec<u
     matrix[coord[0]][coord[1]] = letter;
     println!("{:?}", matrix);
     draw_board(matrix.to_vec());
+    check_if_has_winner(matrix);
     return true;
+}
+
+fn check_diagonal(matrix: &Vec<Vec<String>>) -> bool {
+    let current_letter = &matrix[0][0];
+    if check_if_empty(current_letter) {
+        return false;
+    }
+    for i in 0..matrix.len() {
+        if &matrix[i][i] != current_letter {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn check_reverse_diagonal(matrix: &Vec<Vec<String>>) -> bool {
+    let current_letter = &matrix[matrix.len() - 1][0];
+    if check_if_empty(current_letter) {
+        return false;
+    }
+    for i in (0..matrix.len()).rev() {
+        if &matrix[i][matrix.len() - 1 - i] != current_letter {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn check_row(matrix: &Vec<Vec<String>>) -> bool {
+    let mut response = false;
+    for i in 0..matrix.len() {
+        let current_letter = &matrix[i][0];
+        if check_if_empty(current_letter) {
+            response = false;
+        }
+        for j in 0..matrix.len() {
+            if &matrix[i][j] != current_letter {
+                response = false;
+                break;
+            }
+            if (j == matrix.len() - 1)
+                && &matrix[i][j] == current_letter
+                && !check_if_empty(current_letter)
+            {
+                response = true;
+                break;
+            }
+        }
+        if response {
+            break;
+        }
+    }
+    return response;
+}
+
+fn check_column(matrix: &Vec<Vec<String>>) -> bool {
+    let mut response = false;
+    for i in 0..matrix.len() {
+        let current_letter = &matrix[0][i];
+        for j in 0..matrix.len() {
+            if check_if_empty(&matrix[j][i]) {
+                break;
+            }
+            if &matrix[j][i] != current_letter {
+                response = false;
+                break;
+            }
+            if (j == matrix.len() - 1) && &matrix[j][i] == current_letter {
+                response = true;
+                break;
+            }
+        }
+    }
+    return response;
+}
+
+fn check_if_empty(letter: &String) -> bool {
+    if letter == &" ".to_string() {
+        return true;
+    }
+    return false;
+}
+
+fn check_if_has_winner(matrix: &Vec<Vec<String>>) {
+    if check_diagonal(matrix)
+        || check_reverse_diagonal(matrix)
+        || check_column(matrix)
+        || check_row(matrix)
+    {
+        println!("A player has won!");
+    }
 }
